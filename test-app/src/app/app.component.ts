bf706014ffc1c81d0ceee07b7d68dadc0caff731
Yuliya Meleshko <yuliya.meleshko@itechart-group.com>
2020-06-02 11:09:45 +0300

    Finished menu

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  links: Array<string> = ['Introduction/Overview', 'PowerMeters', 'ShockWiz', 'TyreWiz', 'Service/Support'];
+  tiles: Array<object> = [
+    {title: 'QUARQ: Know Your Powers', body: 'Intoduction to the Quarq brand and history', amount: 1},
+    {title: 'The Heart Of Your Bycicle: Quarq Factory Tour', body: 'Introduction and overview of the Quarq factory', amount: 1},
+    {title: 'Quarq ShockWiz: An Automated Suspension Tuning Device', body: 'Introduction and overview of the Quarq ShockWiz', amount: 1},
+    {title: 'TyreWiz - bicycle tire pressure sensor', body: 'Introduction and overview of the Quarq TyreWiz.', amount: 1},
+    {title: 'Quarq DZero - Reach A Higher Level', body: 'Overview of the Quarq DZero features and technologies', amount: 2}
+  ]
   title = 'test-app';
 }
